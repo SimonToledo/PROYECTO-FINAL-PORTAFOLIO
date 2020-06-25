7140319f96666ba782233d0d0289b4a43df01c9c
Simón Toledo Morales <SIMOM@MacBook-Pro-de-Simon.local>
2020-06-25 13:25:31 -0400

    imagenes

--- portafolio_fotografico/src/views/Home.vue
+++ portafolio_fotografico/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/portada.jpeg" width="15%" style="border-radius: 100%;">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
