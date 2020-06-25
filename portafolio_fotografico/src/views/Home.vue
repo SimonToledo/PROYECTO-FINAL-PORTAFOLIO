505fb02b582b5c6bd3ad8a90c06532106a6f66e0
Simón Toledo Morales <SIMOM@MacBook-Pro-de-Simon.local>
2020-06-25 16:59:03 -0400

    avance

--- portafolio_fotografico/src/views/Home.vue
+++ portafolio_fotografico/src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/portada.jpeg" width="15%" style="border-radius: 100%;">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Simón Toledo"/>
   </div>
 </template>
+
+<style>
+  div{
+    background-color: black;
+  }
+
+</style>
 
 <script>
 // @ is an alias to /src
